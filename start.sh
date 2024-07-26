1be03cae9b74c4eada7864d660abbe807dd0ee5a
Devam42 <dkathane42@gmail.com>
2024-07-26 15:03:25 +0530

    Update

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Google Chrome
+apt-get update
+apt-get install -y wget gnupg
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -y google-chrome-stable
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+# Set correct permissions
+chmod +x /usr/local/bin/chromedriver
+
+# Start the Flask application
+gunicorn app:app --bind 0.0.0.0:4000
